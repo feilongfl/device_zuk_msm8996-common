687121b7a6fca8737bd2a6474c487dc4278502a1
kubersharma001 <kubersharma001@gmail.com>
2020-10-05 14:40:21 +0530

    msm8996-common: Move tethering overlays to TetheringOverlay rro module
    As per AOSP:
    "OEMs that wish to change the below settings must do so via a runtime resource overlay package"
    This is migrated to tethering package from frameworks/base/core/res/res/..
    
    rro_module is based on:
    Author:     Roshan Pius <rpius@google.com>
    Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5
    
    * Cleanup the tether overlays from internal frameworks overlays
    
    Change-Id: Ibe421c40f24301931fc37de826c04e4b3bfecba8

--- msm8996.mk
+++ msm8996.mk
@@ -406,7 +406,8 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf \
-    WifiOverlay
+    WifiOverlay \
+    TetheringOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
